12cb10c2f3e0d780a422d444d6508ea84a688596
Om-Khode <omkhode2003@gmail.com>
2024-02-02 15:55:11 +0530

    Update CrossOrigin origins in TaskController.java

--- src/main/java/com/todolist/todolist/controllers/TaskController.java
+++ src/main/java/com/todolist/todolist/controllers/TaskController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/api/tasks")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class TaskController {
 
     @Autowired
